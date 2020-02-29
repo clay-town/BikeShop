c35cf8a43d452c8ccf34459713e5c3133b1ca5f2
Taylor Slye <slyet@oregonstate.edu>
2020-02-29 14:38:55 -0800

    add reservation database to customer picks up bike

--- customer_picks_up_bike.rb
+++ customer_picks_up_bike.rb
@@ -1,11 +1,10 @@
 #driver program for simulating customer picking up a bike
 require_relative 'customer_picks_up_bike_workflow'
-
+require_relative 'reservation_database'
 
 reservation_database = ReservationDatabase.new
-employee = Employee.new
 
-workflow = CustomerPicksUpBikeWorkflow.new(reservation, employee)
+workflow = CustomerPicksUpBikeWorkflow.new(reservation_database)
 
 workflow.run
 
